f482317dbfab9012ccf0b5736ce9d8c879da7757
Sam <mare.k@gmx.de>
2011-12-08 22:29:17 +0100

    - Some typo - Added ability to search for the content of a chunk

--- search.extender.inc.php
+++ search.extender.inc.php
@@ -22,36 +22,38 @@
 
 $searchFields = isset($searchFields) ? $searchFields : "content";
 /*
-	Param: searchFields
+    Param: searchFields
 
-	Purpose:
- 	Fields to search in
+    Purpose:
+     Fields to search in
 
-	Options:
-	Comma separated list of document variables and template variables
-	
-	Default:
-	"content"
+    Options:
+    Comma separated list of document variables and template variables
+    
+    Default:
+    "content"
 */
 
 $searchOptions = isset($searchOptions) ? $searchOptions : "";
 /*
-	Param: searchOptions
+    Param: searchOptions
 
-	Purpose:
- 	Search Options
+    Purpose:
+     Search Options
 
-	Options:
-	"caseSensitive" - Get case sensitive results only
-	"regex" -   Search for regular expressions
-	"code" -    Code of custom function. The variable $searchContent contains
-	            the content of the document variable or template variable
-	            Example:    &searchOptions=`code` &searchString=`return strpos(strtolower($searchContent), 'test');`
-	                        Should return then same results as
-	                        &searchOptions=`` &searchString=`test`
-	
-	Default:
-	""
+    Options:
+    "caseSensitive" - Get case sensitive results only
+    "regex" -   Search for regular expressions
+    "file" -    Search for the content of a file
+    "chunk" -   Search for the content of a chunk
+    "code" -    Code of custom function. The variable $searchContent contains
+                the content of the document variable or template variable
+                Example:    &searchOptions=`code` &searchString=`return strpos(strtolower($searchContent), 'test');`
+                            Should return then same results as
+                            &searchOptions=`` &searchString=`test`
+    
+    Default:
+    ""
 */
 
 // ---------------------------------------------------
@@ -59,55 +61,55 @@ $searchOptions = isset($searchOptions) ? $searchOptions : "";
 // ---------------------------------------------------
 
 if (!class_exists("searchFilter")) {
-	class searchFilter {
-		var $sourceFields, $searchString, $searchOptions, $caseSensitive;
-	
-		function searchFilter($searchString = "", $sourceFields = "content", $searchOptions = "") {
-			$this->options = array_combine($options = explode(",", $searchOptions), array_fill(0, count($options), true));
-			if ($this->options["caseSensitive"] = (bool) $this->options["caseSensitive"] || false)
-			    $this->searchString = strtolower($this->searchString);
-			
-			$this->options["code"] = isset($this->options["code"]) ? (bool) $this->options["code"] : false;
-			$this->searchString = (isset($this->options["file"]) && (bool) $this->options["file"] ?
-			                        file_exists($searchString) : false) ? file_get_contents($searchString) : $searchString;
-			$this->sourceFields = explode(",", $sourceFields);
-		}
+    class searchFilter {
+        var $sourceFields, $searchString, $searchOptions, $caseSensitive;
+        
+        function searchFilter($searchString = "", $sourceFields = "content", $searchOptions = "") {
+            global $modx;
+            $this->options = array_combine($options = explode(",", $searchOptions), array_fill(0, count($options), true));
+            if ($this->options["caseSensitive"] = $this->options["caseSensitive"] || false)
+                $this->searchString = strtolower($this->searchString);
+            
+            $this->options["code"] = isset($this->options["code"]) ? $this->options["code"] : false;
+            $this->searchString = ($this->options["file"] && ($file = file_get_contents($searchString))) ? $file : (($this->options["chunk"] && ($chunk = $modx->getChunk($searchString))) ? $chunk : $searchString);
+            $this->sourceFields = explode(",", $sourceFields);
+        }
 
-		function execute($resource) {
-		    global $modx;
-			
-			$result = 0;
-			
-			foreach ($this->sourceFields as $field) {
-			    $searchContent = $resource[$field];
-			    
-		        if ($this->options["code"]) {
-		            if (eval($this->searchString) !== false)
-		                $result = 1;
-		        } elseif ($this->options["regex"]) {
-		            $matches = array();
-		            preg_match($this->searchString, $searchContent, $matches);
-		            if (count($matches) > 0)
-		                $result = 1;
-			    } else {
-			        if (!$this->options["caseSensitive"])
-			            $searchContent = strtolower($searchContent);
+        function execute($resource) {
+            global $modx;
+            
+            $result = 0;
+            
+            foreach ($this->sourceFields as $field) {
+                $searchContent = $resource[$field];
+                
+                if ($this->options["code"]) {
+                    if (eval($this->searchString) !== false)
+                        $result = 1;
+                } elseif ($this->options["regex"]) {
+                    $matches = array();
+                    preg_match($this->searchString, $searchContent, $matches);
+                    if (count($matches) > 0)
+                        $result = 1;
+                } else {
+                    if (!$this->options["caseSensitive"])
+                        $searchContent = strtolower($searchContent);
 
-			        if (strpos($searchContent, $this->searchString) !== false)
-				        $result = 1;
+                    if (strpos($searchContent, $this->searchString) !== false)
+                        $result = 1;
                 }
-			}
-			return $result;
-		}
-	}
+            }
+            return $result;
+        }
+    }
 }
 
 // ---------------------------------------------------
 // Search Filter Execution
 // ---------------------------------------------------
 if (isset($searchString)) {
-	$searchFilterObject = new searchFilter($searchString, $searchFields, $searchOptions);
-	$filters["custom"]["searchFilter"] = array($searchFields,array($searchFilterObject,"execute"));
+    $searchFilterObject = new searchFilter($searchString, $searchFields, $searchOptions);
+    $filters["custom"]["searchFilter"] = array($searchFields,array($searchFilterObject,"execute"));
 }
 
 ?>
